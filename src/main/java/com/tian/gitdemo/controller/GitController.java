16a96c87f0fa5cc95ed3af4b2f15f9826bb35b89
田志恒 <453780260@qq.com>
2023-01-03 14:56:33 +0800

    第一次提交代码

---
+++ src/main/java/com/tian/gitdemo/controller/GitController.java
@@ -0,0 +1,12 @@
+package com.tian.gitdemo.controller;
+
+/**
+ * @author tzhiheng
+ * @date 2023-01-03 - 14:47
+ * @description
+ */
+public class GitController {
+    public static void main(String[] args) {
+        System.out.println("少年歌行");
+    }
+}
