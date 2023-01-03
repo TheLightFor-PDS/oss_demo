23ad2872d75980b6ceb6ef16cd530c9f110caa9e
田志恒 <453780260@qq.com>
2023-01-03 15:21:55 +0800

    第三次提交

--- src/main/java/com/tian/gitdemo/controller/GitController.java
+++ src/main/java/com/tian/gitdemo/controller/GitController.java
@@ -9,5 +9,10 @@ public class GitController {
     public static void main(String[] args) {
         System.out.println("少年歌行");
         System.out.println("江南霹雳堂 雷家 雷无桀");
+        System.out.println("我欲乘风向北行,雪落轩辕大如席。 " +
+                "我欲借船向东游,绰约仙子迎风立。 " +
+                "我欲踏云千万里,庙堂龙吟奈我何? " +
+                "昆仑之巅沐日光,沧海绝境见青山。 " +
+                "长风万里燕归来,不见天涯人不回!");
     }
 }
